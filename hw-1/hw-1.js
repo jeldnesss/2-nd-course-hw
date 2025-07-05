110af0826603d6dd1de63f7b7fbb4d30e10e9cbc
julia <juliaarlovskaya@mail.ru>
2025-07-05 18:29:37 +0300

    js tasks

---
+++ hw-1/hw-1.js
@@ -0,0 +1,59 @@
+/* task1*/
+let a = 10;
+alert(a);
+
+a = 20;
+alert(a);
+
+
+/* task2*/
+const ageIphone = 2007;
+alert(ageIphone);
+
+/* task3*/
+const nameJavaScript = "Брендан Айк";
+alert(nameJavaScript);
+
+/* task4*/
+let num1 = 10;
+let num2 = 2;
+alert(num1 + num2);
+alert(num1 - num2);
+alert(num1 * num2);
+alert(num1 % num2);
+
+/* task5*/
+let number = 2;
+let result = number ** 2;
+alert(result);
+
+/* task6*/
+let b = 9;
+let c = 2;
+alert(b % c);
+
+/* task7*/
+let num = 1;
+num += 5;
+num -= 3;
+num *= 7;
+num /= 3;
+num++;
+num--;
+alert(num);
+
+/*task8*/
+let age = prompt("Сколько вам лет?");
+alert(age);
+
+/*task9*/
+const user = {
+    name: "Julia",
+    age: 18,
+    isAdmin: true
+}
+
+/*task10*/
+
+let name = prompt("Как вас зовут?");
+alert(`Привет, ${name} !`)
